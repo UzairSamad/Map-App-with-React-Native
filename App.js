ea864ad184aa3adb5846455af45d6dc201fa9500
M.Uzair <uzair.iu17@gmail.com>
2020-05-13 00:15:45 +0500

    asked permission fro use

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 import Header from './src/component/Header'
 import Home from './src/component/Home'
+import Geolocation from '@react-native-community/geolocation';
 
 
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
 
 class App extends Component {
-  render() {
+    getUserLocationHandler = () => {
+        // navigator.geolocation.getCurrentPosition(postion => {
+        //   const location = JSON.stringify(postion)
+        //   console.log(location)
+        // }, err =>  console.log(err) )
+        Geolocation.getCurrentPosition(info => console.log(info));
 
-    return (
-      <View>
-        <Header />
-        <Home />
-      </View>
+    }
 
-    )
-  }
+    render() {
+
+        return (
+            <View style={styles.container}>
+                <Home onGetLocation={this.getUserLocationHandler} />
+            </View>
+
+        )
+    }
 }
 
 export default App
--- src/component/Home.js
+++ src/component/Home.js
@@ -4,13 +4,13 @@ import { View, Text, Button, TouchableOpacity } from 'react-native'
 class Home extends Component {
 
     render() {
+        console.log(this.props)
         return (
             <View >
-                <Text style={{ fontSize: 25, margin: 5 }}>Home Screen</Text>
-                <Button title='press me' />
-                <TouchableOpacity  style={{backgroundColor:'black',width:80,height:50,marginLeft:150,marginTop:30,borderRadius:3}}>
+                <Button title='Fetch location' onPress={this.props.onGetLocation} />
+                {/* <TouchableOpacity  style={{backgroundColor:'black',width:80,height:50,marginLeft:150,marginTop:30,borderRadius:3}}>
                     <Text style={{color:'white',display:'flex',padding:8}}>Press me</Text>
-                </TouchableOpacity>
+                </TouchableOpacity> */}
             </View>
         )
     }
